dc079c8c5640d0dae9cf36cbfc7ad139a85a339c
ssalpyo <tjdvy6@gmail.com>
2022-07-02 23:07:37 +0900

    # Add: import antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
