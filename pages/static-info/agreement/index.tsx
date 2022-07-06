d724a22d7a41cf2a805f7717e3201f365f5a7bd4
dhzmqjf <dhzmqjf@naver.com>
2022-07-06 20:15:26 +0900

    #Add: 이용약관 페이지 추가

---
+++ pages/static-info/agreement/index.tsx
@@ -0,0 +1,6 @@
+import AgreementContainer from "@/src/components/units/static-info/agreement/Agreement.container";
+import React from "react";
+
+export default function index() {
+  return <AgreementContainer />;
+}
---
+++ pages/static-info/index.tsx
@@ -0,0 +1,5 @@
+import StaticInfoContainer from "@/src/components/units/static-info/StaticInfo.container";
+
+export default function index() {
+  return <StaticInfoContainer />;
+}
