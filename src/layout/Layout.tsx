7fb5ba75823a0dc45aacc59ad631d286c752ce6a
ssssksss <ssssksss@gmail.com>
2022-07-07 16:21:03 +0900

    # Update: [레이아웃] 특정 URL 경로를 제외하기 위해 작성

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,17 +2,27 @@ import { ReactNode } from "react";
 import styled from "@emotion/styled";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import { useRouter } from "next/router";
 
 interface ILayoutProps {
   children: ReactNode;
 }
 
 const Layout = (props: ILayoutProps) => {
+  const router = useRouter();
+
+  console.log(router.asPath.split("/")[1] === "host");
   return (
     <>
-      <Header />
-      <Body> {props.children} </Body>
-      <Footer />
+      {router.asPath.split("/")[1] === "host" ? (
+        <div> {props.children} </div>
+      ) : (
+        <>
+          <Header />
+          <Body> {props.children} </Body>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
