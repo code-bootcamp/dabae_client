88296dc40e958e95582dd689cf3435d6e84349d3
dhzmqjf <dhzmqjf@naver.com>
2022-07-07 13:13:47 +0900

    개인정보 처리방침 페이지 추가

--- pages/static-info/privacy/index.tsx
+++ pages/static-info/privacy/index.tsx
@@ -1,5 +1,6 @@
+import PrivacyContainer from "@/src/components/units/static-info/privacy/Privacy,container";
 import React from "react";
 
 export default function index() {
-  return <div>index</div>;
+  return <PrivacyContainer />;
 }
