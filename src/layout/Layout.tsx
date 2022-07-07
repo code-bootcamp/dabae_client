3815cd360283970feb26ab3f557a76e32f0ebf4d
dhzmqjf <dhzmqjf@naver.com>
2022-07-07 17:03:47 +0900

    #Add: 호스트 페이지 Layout 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,17 +2,34 @@ import { ReactNode } from "react";
 import styled from "@emotion/styled";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import { useRouter } from "next/router";
 
 interface ILayoutProps {
   children: ReactNode;
 }
 
+const HIDDEN_HEADERS = ["/joinhost"];
+
 const Layout = (props: ILayoutProps) => {
+  const router = useRouter();
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+  console.log(router.asPath.split("/")[1] === "host");
   return (
     <>
-      <Header />
-      <Body> {props.children} </Body>
-      <Footer />
+      {router.asPath.split("/")[1] === "host" ? (
+        <div> {props.children} </div>
+      ) : (
+        <>
+          <Header />
+          {isHiddenHeader ? (
+            <div> {props.children} </div>
+          ) : (
+            <Body> {props.children} </Body>
+          )}
+          <Footer />
+        </>
+      )}
     </>
   );
 };
