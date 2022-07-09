55ee77dab8788d0f8e7c8c58b8c9a47ba0e5114a
ssssksss <ssssksss@gmail.com>
2022-07-10 00:27:57 +0900

    Update: [호스트] 호스트 사진 drag and drop, 미리보기 추가, react hook form을 이용하여 shllow routing을 하여도 입력한 상태값 유지

--- src/components/units/host/class/create/HostClassCreate.container.tsx
+++ src/components/units/host/class/create/HostClassCreate.container.tsx
@@ -1,6 +1,7 @@
 import HostClassCreateUI from "./HostClassCreate.presenter";
-import { useDropzone } from "react-dropzone";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { dateFormat4y2m2d2h2d } from "@/src/function/date/format/dateFormat";
 
 /**
  * Author : Sukyung Lee
@@ -9,23 +10,70 @@ import { useState } from "react";
  * Description : 호스트 수업 등록 컨테이너
  */
 const HostClassCreate = () => {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
   const [step, setStep] = useState(1);
-  const files = acceptedFiles.map((file: any) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+  const methods = useForm<any>({
+    defaultValues: {
+      tags: [],
+      images: [],
+      classRecruitDeadLine: ["", ""],
+    },
+  });
+  const [firstCategory, setFirstCategory] = useState("");
+  const [secondCategory, setSecondCategory] = useState("");
+  // const [classRecruitDeadLine, setClassRecruitDeadLine] = useState([
+  //   dateFormat4y2m2d2h2d(new Date()),
+  //   dateFormat4y2m2d2h2d(new Date()),
+  // ]);
+  const [startClassDate, setStartClassDate] = useState([
+    dateFormat4y2m2d2h2d(new Date()),
+    dateFormat4y2m2d2h2d(new Date()),
+  ]);
+  const onChangeFirstCategory = (e: any) => {
+    setFirstCategory(e.target.value);
+    methods.setValue("firstCategory", e.target.value);
+  };
+  const onChangeSecondCategory = (e: any) => {
+    setSecondCategory(e.target.value);
+    methods.setValue("secondCategory", e.target.value);
+  };
+
   const onClickChangeStep = (move: number) => () => {
     setStep(move);
   };
+
+  const onChangeClassRecruitDeadLine = (date: any, dateString: string) => {
+    // date, dateString
+    // setClassRecruitDeadLine([dateString[0], dateString[1]]);
+    methods.setValue("classRecruitDeadLine", [dateString[0], dateString[1]]);
+  };
+
+  const onChangeStartClassDate = (date: any, dateString: string) => {
+    // date, dateString
+    setStartClassDate([dateString[0], dateString[1]]);
+    methods.setValue("startClassDate", [dateString[0], dateString[1]]);
+  };
+
+  const onChangeLevel = (e: any) => {
+    methods.setValue("level", e.target.value);
+  };
+
+  const onClickSubmit = () => {};
+
   return (
     <HostClassCreateUI
-      getRootProps={getRootProps}
-      getInputProps={getInputProps}
-      files={files}
       step={step}
       onClickChangeStep={onClickChangeStep}
+      onChangeFirstCategory={onChangeFirstCategory}
+      onChangeSecondCategory={onChangeSecondCategory}
+      firstCategory={firstCategory}
+      secondCategory={secondCategory}
+      onChangeClassRecruitDeadLine={onChangeClassRecruitDeadLine}
+      // classRecruitDeadLine={classRecruitDeadLine}
+      onChangeStartClassDate={onChangeStartClassDate}
+      startClassDate={startClassDate}
+      methods={methods}
+      onClickSubmit={onClickSubmit}
+      onChangeLevel={onChangeLevel}
     />
   );
 };
