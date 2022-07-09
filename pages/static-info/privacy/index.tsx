ef7c6d1d532cb77789f522b41f50aa2b4fc80ff7
dhzmqjf <dhzmqjf@naver.com>
2022-07-09 18:51:54 +0900

    #Add: privacy 컴포넌트 파일명 변경

--- pages/static-info/privacy/index.tsx
+++ pages/static-info/privacy/index.tsx
@@ -1,4 +1,4 @@
-import PrivacyContainer from "@/src/components/units/my/static-info/privacy/Privacy,container";
+import PrivacyContainer from "@/src/components/units/my/static-info/privacy/Privacy.container";
 
 export default function index() {
   return <PrivacyContainer />;
