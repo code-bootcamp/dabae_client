1a1bab99a33d8a6425de5f0aa4328d4f7ecb0825
ssalpyo <tjdvy6@gmail.com>
2022-07-11 16:19:41 +0900

    # Add: Top 버튼 컴포넌트

---
+++ src/components/commons/top-button/index.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+export default function TopButton() {
+  const moveToTop = () => {
+    document.documentElement.scrollTop = 0;
+  };
+
+  return <Button onClick={moveToTop}>Top</Button>;
+}
+
+const Button = styled.button`
+  display: block;
+  position: fixed;
+  z-index: 2;
+  bottom: 150px;
+  right: 15%;
+  font-size: 20px;
+  padding: 20px;
+  border: none;
+  border-radius: 20px;
+  background-color: #32c2b9;
+  color: #fff;
+
+  @media (max-width: 1500px) {
+    right: 3%;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
