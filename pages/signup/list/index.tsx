1746b41a6f76dd4a879d3fca1a5a27afd663b6bb
hyokyungAn <ahyok1106@gmail.com>
2022-07-11 23:01:21 +0900

    # Add: 회원가입 리스트

---
+++ pages/signup/list/index.tsx
@@ -0,0 +1,5 @@
+import SignUpListContainerPage from "@/src/components/units/signup/list/SignUpList.container";
+
+export default function SignUplistPage() {
+  return <SignUpListContainerPage />;
+}
