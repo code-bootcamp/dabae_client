ce6f7ea3bfbd1b7da56973c156caf5c9dc2b943a
dhzmqjf <dhzmqjf@naver.com>
2022-07-14 20:33:28 +0900

    #Add: 메인 카드 컴포넌트 분할

--- src/components/commons/productcard/ProductCard.container.tsx
+++ src/components/commons/productcard/ProductCard.container.tsx
@@ -1,5 +1,12 @@
 import ProductCardPresenter from "./ProductCard.presenter";
+import { useState } from "react";
 
 export default function ProductCardContainer() {
-  return <ProductCardPresenter />;
+  const [isActive, setIsActive] = useState(false);
+
+  const onClickLike = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  return <ProductCardPresenter isActive={isActive} onClickLike={onClickLike} />;
 }
