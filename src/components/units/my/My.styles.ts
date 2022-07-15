dcd5a8c6c3d5c58edb6305c01b45a1932b3c1317
dhzmqjf <dhzmqjf@naver.com>
2022-07-15 14:28:08 +0900

    #Add: 마이페이지 수정 및 그래프ql 데이터 추가

--- src/components/units/my/My.styles.ts
+++ src/components/units/my/My.styles.ts
@@ -49,7 +49,7 @@ export const ProfileContentBox = styled.a`
   width: 100%;
   box-shadow: 4px 4px 6px 0 rgb(0 0 0 / 10%);
   border-radius: 10px;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
