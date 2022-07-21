dd1e38a71a7eccb741e2998110d8c1ccfa64ad6d
ssalpyo <tjdvy6@gmail.com>
2022-07-21 12:44:14 +0900

    # Add: 상품 상세 페이지: fetchCourse data 연결

--- src/components/units/product/detail/detail-contents/ProductDetailContents.presenter.tsx
+++ src/components/units/product/detail/detail-contents/ProductDetailContents.presenter.tsx
@@ -1,6 +1,7 @@
 import KakaoMapDetail from "@/src/components/commons/kakao-map";
 import ProductCardContainer from "@/src/components/commons/productcard/ProductCard.container";
 import { CF } from "@/styles/commonComponentStyle";
+import DOMPurify from "dompurify";
 import * as S from "./ProductDetailContents.styles";
 import { IProductDetailContentsUIProps } from "./ProductDetailContents.types";
 
@@ -13,7 +14,7 @@ export default function ProductDetailContentsUI(
         <S.Label>다배 정보</S.Label>
         <CF.RowDiv>
           <S.MiniLabel>난이도</S.MiniLabel>
-          <S.Info>보통</S.Info>
+          <S.Info>{props.data?.difficulty}</S.Info>
         </CF.RowDiv>
         <CF.RowDiv>
           <S.MiniLabel>사용기간</S.MiniLabel>
@@ -23,12 +24,21 @@ export default function ProductDetailContentsUI(
       <S.SectionWrapper>
         <S.Label>다배 소개</S.Label>
         <S.IntroImage src="/images/product_detail/intro_1.webp" />
+        {typeof window !== "undefined" ? (
+          <S.Contents
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(String(props.data?.contents)),
+            }}
+          />
+        ) : (
+          <></>
+        )}
       </S.SectionWrapper>
       <S.SectionWrapper>
         <S.Label>진행 장소</S.Label>
-        <KakaoMapDetail address="서울 영등포구 여의동로 지하 343" />
+        <KakaoMapDetail address={props.data?.address} />
         <S.LocationTitle>
-          서울시 용산구 이태원로 242, 트렌드바이미 (한강직연 3번출구 5분 직진)
+          {props.data?.address} {props.data?.addressDetail}
         </S.LocationTitle>
       </S.SectionWrapper>
       <S.SectionWrapper>
