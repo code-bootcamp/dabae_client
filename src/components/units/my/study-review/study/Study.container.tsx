ed7d5d6b2d7cd6565fdbd8e80e0741d62ec96c38
dhzmqjf <dhzmqjf@naver.com>
2022-07-24 22:52:32 +0900

    #Add: 수강신청 확인 목록 리스트 페이지  추가

--- src/components/units/my/study-review/study/Study.container.tsx
+++ src/components/units/my/study-review/study/Study.container.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
 import React from "react";
+import StudyPresneter from "./Study.presneter";
 
 export default function StudyContainer() {
-  return <div>Study.container</div>;
+  const router = useRouter();
+
+  const BackMyMove = () => {
+    router.push("/my/study-review");
+  };
+
+  const reviewMove = () => {
+    router.push("/my/study-review/reivew/write");
+  };
+
+  return <StudyPresneter BackMyMove={BackMyMove} reviewMove={reviewMove} />;
 }
