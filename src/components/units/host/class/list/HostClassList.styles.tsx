2cc9e6dedc318aca0218a4b430804da7737a22d1
ssssksss <ssssksss@gmail.com>
2022-08-02 00:43:24 +0900

    #Fix [호스트-수업등록목록] 호스트가 등록한 수업 목록 조회

---
+++ src/components/units/host/class/list/HostClassList.styles.tsx
@@ -0,0 +1,270 @@
+// import theme from "@/styles/theme";
+import theme from "@/styles/theme";
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { DatePicker } from "antd";
+
+const { RangePicker } = DatePicker;
+
+const fadeOutAnimation = keyframes`
+        from {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+        }
+`;
+export const Container = styled.section`
+  scroll-behavior: smooth;
+  animation: ${fadeOutAnimation} 1s linear;
+  display: flex;
+  flex-flow: nowrap column;
+  padding: 10px;
+  gap: 10px;
+
+  label,
+  input,
+  select {
+    cursor: pointer;
+  }
+`;
+export const SearchHeader = styled.div`
+  display: flex;
+  flex-flow: nowrap column;
+  gap: 10px;
+  background-color: #fff;
+  padding: 10px 10px;
+`;
+export const SearchSelect = styled.select`
+  height: 30px;
+  padding: 0px 4px;
+`;
+export const SearchDateButton = styled.button`
+  height: 30px;
+  outline: 2px solid #32c2b9;
+  color: #32c2b9;
+  padding: 2px 10px;
+  border-radius: 10px;
+`;
+
+export const RowCenterDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BorderDiv = styled.div`
+  width: 100%;
+  border: solid ${theme.backgroundColors.border} 1px;
+  padding: 2px 0px;
+  font-size: 16px;
+  font-weight: 600;
+`;
+export const RowCenterProgressStatusDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+
+  @media (max-width: 768px) {
+    width: 100px;
+    padding: 2px 0px;
+  }
+`;
+
+export const RowCenterButtonDiv = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 120px;
+  font-size: 16px;
+
+  @media (max-width: 768px) {
+    width: 100px;
+    padding: 2px 0px;
+  }
+`;
+export const RowCenterOperationDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 120px;
+`;
+export const OperatingDate = styled.div`
+  display: flex;
+  flex-flow: nowrap column;
+  min-width: 120px;
+  align-items: center;
+
+  @media (max-width: 1023px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+export const InputClassTitle = styled.div`
+  width: 120px;
+  height: 30px;
+  outline: solid 1px #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const InputSearch = styled.input``;
+
+export const RangePickerStyle = styled(RangePicker)``;
+
+//  /////////////////////////////////////
+
+export const ManageHeaderDiv = styled.div`
+  color: #32c2b9;
+  display: grid;
+  height: 40px;
+  align-items: center;
+  background-color: #fff;
+  grid-template-columns: 60px calc(100% - 380px) 200px 120px;
+  font-size: 16px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 60px calc(100% - 200px) 80px 60px;
+  }
+
+  @media (max-width: 1200px) {
+    grid-template-columns: 60px calc(100% - 240px) 120px 60px;
+  }
+`;
+
+export const RowCenterHeaderDiv1 = styled.div`
+  min-width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 768px) {
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+
+export const RowCenterHeaderDiv2 = styled.div`
+  min-width: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 768px) {
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+export const RowCenterHeaderDiv3 = styled.div`
+  min-width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 1200px) {
+    min-width: 100px;
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+export const RowCenterHeaderDiv4 = styled.div`
+  min-width: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 768px) {
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+
+export const ManageBodyDiv = styled.div`
+  display: grid;
+  font-size: 16px;
+  font-weight: 600;
+  height: 80px;
+  grid-template-columns: 60px calc(100% - 380px) 200px 120px;
+  background-color: #fff;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 60px calc(100% - 200px) 80px 60px;
+  }
+
+  @media (max-width: 1200px) {
+    grid-template-columns: 60px calc(100% - 240px) 120px 60px;
+    font-size: 12px;
+    height: 60px;
+  }
+`;
+
+export const RowCenterBodyDiv1 = styled.div`
+  min-width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 768px) {
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+
+export const RowCenterBodyDiv2 = styled.div`
+  min-width: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 4px;
+
+  @media (max-width: 768px) {
+    flex-flow: nowrap column-reverse;
+    font-size: 12px;
+  }
+`;
+export const RowCenterBodyDiv3 = styled.div`
+  min-width: 200px;
+  gap: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  line-height: 10px;
+
+  @media (max-width: 1200px) {
+    min-width: 100px;
+    flex-flow: nowrap column;
+    font-size: 12px;
+  }
+`;
+export const RowCenterBodyDiv4 = styled.div`
+  display: flex;
+  flex-flow: nowrap column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PaginationDiv = styled.div`
+  width: 100%;
+  height: 40px;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
