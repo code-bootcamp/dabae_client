c15e0ee3db8cd727a51f3b22392d3d2a080b4773
ssalpyo <tjdvy6@gmail.com>
2022-08-01 16:10:09 +0900

    # Add: 로그인 권한분기

--- pages/save-list/index.tsx
+++ pages/save-list/index.tsx
@@ -1,5 +1,7 @@
+import { useAuth } from "@/src/components/commons/hooks/useAuth";
 import SaveListContainer from "@/src/components/units/save-list/SaveList.container";
 
 export default function index() {
+  useAuth();
   return <SaveListContainer />;
 }
--- src/components/commons/hooks/useAuth.tsx
+++ src/components/commons/hooks/useAuth.tsx
@@ -1,29 +1,16 @@
 import { Modal } from "antd";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-// import { getAccessToken } from "../../../commons/libraries/getAccessToken";
-import { useRecoilValueLoadable } from "recoil";
-import { restoreAccessTokenLoadable } from "../../../commons/store";
+import { getAccessToken } from "../../../commons/libraries/getAccessToken";
+// import { useRecoilValueLoadable } from "recoil";
+// import { restoreAccessTokenLoadable } from "../../../commons/store";
 
 export function useAuth() {
   const router = useRouter();
-  const value = useRecoilValueLoadable(restoreAccessTokenLoadable);
-
-  // useEffect(() => {
-  //   getAccessToken().then((newAccessToken) => {
-  //     if (!newAccessToken) {
-  //       Modal.error({
-  //         content: "로그인 후 이용 가능합니다.",
-  //         onOk() {
-  //           router.push("/login");
-  //         },
-  //       });
-  //     }
-  //   });
-  // }, []);
+  // const value = useRecoilValueLoadable(restoreAccessTokenLoadable);
 
   useEffect(() => {
-    value.toPromise().then((newAccessToken) => {
+    getAccessToken().then((newAccessToken) => {
       if (!newAccessToken) {
         Modal.error({
           content: "로그인 후 이용 가능합니다.",
@@ -34,4 +21,17 @@ export function useAuth() {
       }
     });
   }, []);
+
+  // useEffect(() => {
+  //   value.toPromise().then((newAccessToken) => {
+  //     if (!newAccessToken) {
+  //       Modal.error({
+  //         content: "로그인 후 이용 가능합니다.",
+  //         onOk() {
+  //           router.push("/login");
+  //         },
+  //       });
+  //     }
+  //   });
+  // }, []);
 }
