d0575e32008982621246074f0c2715522eb1e61a
ssalpyo <tjdvy6@gmail.com>
2022-09-13 15:15:57 +0900

    # Fix: 비밀번호 찾기: 권한분기 제거

--- pages/find-password/index.tsx
+++ pages/find-password/index.tsx
@@ -1,7 +1,5 @@
-import { useAuth } from "@/src/components/commons/hooks/useAuth";
 import FindPassword from "@/src/components/units/find-password/FindPassword.container";
 
 export default function FindPasswordPage() {
-  useAuth();
   return <FindPassword />;
 }
