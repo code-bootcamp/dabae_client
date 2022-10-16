b07127465598f88f1adeda2dca4a35c70301dd29
ssalpyo <tjdvy6@gmail.com>
2022-10-17 00:43:43 +0900

    # Del: 콘솔로그 삭제

--- src/components/units/payment/complete/Complete.container.tsx
+++ src/components/units/payment/complete/Complete.container.tsx
@@ -4,7 +4,6 @@ import { FETCH_PAYMENT } from "./Complete.queries";
 import { ICompleteContainer } from "./Complete.types";
 
 export default function CompleteContainer(props: ICompleteContainer) {
-  console.log(props.paymentId);
   const { data } = useQuery(FETCH_PAYMENT, {
     variables: {
       paymentId: props.paymentId,
